e0ddbb11d5bd81f59b4c286cb947b407cbd694a8
zhouyuanhua <zyhwork16@163.com>
2018-05-20 12:51:37 +0800

    Optimize read XMl to treeView

--- QDomxmlParse/corexmlparse.cpp
+++ QDomxmlParse/corexmlparse.cpp
@@ -47,19 +47,28 @@ int CoreXmlParse::readNode(QDomNode node, int deepCount, QStandardItem* item)
     if(!node.isNull() && node.isElement()){
         QDomElement e = node.toElement();
         QDomNodeList list = e.childNodes();
+        int flag=1;//用于判断传入的Item是否为空值
         if(item == NULL){
             item = new QStandardItem(e.tagName());
             model->appendRow(item);
+            flag=0;
         }
         int tempCount = list.count();
-        if(tempCount<=1){
-            QStandardItem* itemChild = new QStandardItem(e.tagName());
-            item->appendRow(itemChild);
-            item->setChild(itemChild->index().row(),1,new QStandardItem(e.text()));
+        if(tempCount==1){
+            if(flag){
+                QStandardItem* itemChild = new QStandardItem(e.tagName());
+                item->appendRow(itemChild);
+                item->setChild(itemChild->index().row(),1,new QStandardItem(e.text()));
+            }
             qDebug()<<"   "<<e.tagName()<<":"<<e.text();
         }else{
-            QStandardItem* itemChild = new QStandardItem(e.tagName());
-            item->appendRow(itemChild);
+            QStandardItem* itemChild;
+            if(flag){
+                itemChild = new QStandardItem(e.tagName());
+                item->appendRow(itemChild);
+            }else{
+                itemChild =item;
+            }
             qDebug()<<deepCount++<<e.tagName();
             for(int i=0;i<tempCount;i++){
                 readNode(list.at(i),deepCount,itemChild);
